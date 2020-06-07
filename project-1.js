70dac9a76cd92b6467c7129addf424fe655f6fc3
DArbatskiy <66027751+DArbatskiy@users.noreply.github.com>
2020-06-08 02:41:13 +0300

    Update project-1.js

--- project-1.js
+++ project-1.js
@@ -1,33 +1,38 @@
 function retranslateToDNA(dna) {
-   let rna = '';
-   dna = dna.toUpperCase();
-   for (let i=0; i< dna.length; i++) {
-       if (dna[i] === 'G') {
-           rna += 'C';
-       } else if (dna[i] === 'C') {
-           rna += 'G';
-       } else if (dna[i] === 'T') {
-           rna += 'A';
-       } else if (dna[i] === 'A') {
-           rna += 'U';
-       } else return 'DNA must contain G, C, T, A';
-   }
-   return rna;
+  let rna = '';
+  dna = dna.toString();
+  dna = dna.toUpperCase();
+  for (let i=0; i< dna.length; i++) {
+      if (dna[i] === 'G') {
+          rna += 'C';
+      } else if (dna[i] === 'C') {
+          rna += 'G';
+      } else if (dna[i] === 'T') {
+          rna += 'A';
+      } else if (dna[i] === 'A') {
+          rna += 'U';
+      } else  {
+        throw 'INVALID_SOURCE_DATA';
+      }
+  }
+  return rna;
 }
 
 function retranslateToRNA(rna) {
-   let dna = '';
-   rna = rna.toUpperCase();
-   for (let i=0; i< rna.length; i++) {
-       if (rna[i] === 'C') {
-           dna += 'G';
-       } else if (rna[i] === 'G') {
-           dna += 'C';
-       } else if (rna[i] === 'A') {
-           dna += 'T';
-       } else if (rna[i] === 'U') {
-           dna += 'A';
-       } else return 'RNA must contain C, G, A, U';
-   }
-   return dna;
+  let dna = '';
+  rna = rna.toUpperCase();
+  for (let i=0; i< rna.length; i++) {
+      if (rna[i] === 'C') {
+          dna += 'G';
+      } else if (rna[i] === 'G') {
+          dna += 'C';
+      } else if (rna[i] === 'A') {
+          dna += 'T';
+      } else if (rna[i] === 'U') {
+          dna += 'A';
+      } else {
+        throw 'INVALID_SOURCE_DATA';
+      }
+  }
+  return dna;
 }
